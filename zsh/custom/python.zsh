d342625b877c44bee7033d8b65d47d758411800a
Martin Mrose <github@penthranet.de>
2014-11-10 17:34:36 +0100

    Added virtualenvwrapper support

--- zsh/custom/python.zsh
+++ zsh/custom/python.zsh
@@ -5,3 +5,9 @@ alias pms='python manage.py shell'
 alias pi='pip install'
 alias pf='pip freeze'
 alias pfg='pip freeze | grep -i'
+
+# virtualenvwrapper
+if [ -e /usr/local/bin/virtualenvwrapper.sh ]; then
+    source /usr/local/bin/virtualenvwrapper.sh
+    export WORKON_HOME=$HOME/virtualenvs
+fi
