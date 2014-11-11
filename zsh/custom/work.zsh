64dabda60d4d716e06ec90ba6e2ab61c545a70ca
Martin Mrose <github@penthranet.de>
2014-11-11 08:45:02 +0100

    Added functions for work

--- zsh/custom/work.zsh
+++ zsh/custom/work.zsh
@@ -1,1 +1,19 @@
-alias np='/cygdrive/c/"Program Files (x86)"/Notepad++/notepad++.exe'
+if [[ `hostname` == W4DEUMSY9002045 ]]; then
+    export http_proxy="http://proxy.mms-dresden.de:8080"
+    export https_proxy=$http_proxy
+    export ftp_proxy=$http_proxy
+
+    alias np='/cygdrive/c/"Program Files (x86)"/Notepad++/notepad++.exe'
+    alias poedit="/cygdrive/c/Program\ Files\ \(x86\)/Poedit/Poedit.exe"
+
+    vms() {
+        tmux send-keys 'vagrant ssh app -- -t "cd /usr/local/*suite*; mediasuite/bin/manage runserver 8100" ";" exec /bin/bash' C-m
+        tmux split-window -c "$PWD"
+        tmux send-keys 'vagrant ssh app -- -t "cd /usr/local/*suite*; mediasuite/bin/manage runcelery -Bv2 worker" ";" exec /bin/bash' C-m
+    }
+    vws() {
+        tmux send-keys 'vagrant ssh app -- -t "cd /usr/local/*suite*; webcastsuite/bin/manage runserver 8100" ";" exec /bin/bash' C-m
+        tmux split-window -c "$PWD"
+        tmux send-keys 'vagrant ssh app -- -t "cd /usr/local/*suite*; webcastsuite/bin/manage runcelery -Bv2 worker" ";" exec /bin/bash' C-m
+    }
+fi
