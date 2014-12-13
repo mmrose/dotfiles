0bf3a144d83647bfe8112e573568c5ba3543caa6
Martin Mrose <github@penthranet.de>
2014-12-13 16:45:24 +0100

    Added scripts folder and chat script

---
+++ scripts/scripts.symlink/chat.sh
@@ -0,0 +1,17 @@
+# #!/usr/bin/bash
+SESSION=chat
+alias tmux="tmux -2"
+
+# if the session is already running, just attach to it.
+tmux has-session -t $SESSION
+if [ $? -eq 0 ]; then
+    echo "Session $SESSION already exists. Attaching."
+    sleep 1
+    tmux attach -t $SESSION
+else
+    tmux new-session -d -s $SESSION 'profanity -a google'
+    tmux selectp -t $SESSION
+    tmux split-window -h 'profanity -a facebook'
+    tmux selectp -t 1
+    tmux attach -t $SESSION
+fi
