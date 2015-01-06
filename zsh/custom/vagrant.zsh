62fa892e583a0853c176da0e9fb91ff413bda0b1
Martin Mrose <github@penthranet.de>
2015-01-06 10:30:05 +0100

    Added command for creating local.zsh in simple pyrun boxes

--- zsh/custom/vagrant.zsh
+++ zsh/custom/vagrant.zsh
@@ -17,3 +17,13 @@ vdu() {
     vagrant up $1
     vagrant ssh -c "update-dotfiles" $1
 }
+
+vsp() {
+    vagrant up $1
+    vagrant ssh -c 'cat << "EOF" > /home/vagrant/.dotfiles/zsh/custom/local.zsh
+export PATH=/home/vagrant/virtualenv/bin:$PATH
+export STAGE=vagrant
+cd /vagrant
+EOF
+'
+}
