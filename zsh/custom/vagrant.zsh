42f220aee0faab462f93f5a51c9f6ab6863b0f82
Martin Mrose <github@penthranet.de>
2015-01-21 10:41:29 +0100

    Added vagrant ssh

--- zsh/custom/vagrant.zsh
+++ zsh/custom/vagrant.zsh
@@ -18,6 +18,7 @@ function vdi() {
     vagrant up $1
     vagrant ssh -c "curl -L https://raw.githubusercontent.com/mmrose/dotfiles/master/install.sh | sh"  $1
     vagrant ssh -c "sudo chsh -s /bin/zsh vagrant" $1
+    vagrant ssh $1
 }
 
 function vdu() {
