9c8b7454480c2eeeb049f24e8793632f80bf6bed
Martin Mrose <github@penthranet.de>
2015-04-13 13:13:49 +0200

    Shortened update dotfiles

--- zsh/custom/dotfiles.zsh
+++ zsh/custom/dotfiles.zsh
@@ -1,3 +1,3 @@
 export PATH="$HOME/.scripts:$PATH"
 
-alias update-dotfiles='curl -L https://raw.githubusercontent.com/mmrose/dotfiles/master/install.sh | sh'
+alias upd='curl -L https://raw.githubusercontent.com/mmrose/dotfiles/master/install.sh | sh'
