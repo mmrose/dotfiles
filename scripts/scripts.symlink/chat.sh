b2e5c54653edb2613f03b0498989ee3c84bd285b
Martin Mrose <github@penthranet.de>
2015-04-27 20:53:10 +0200

    Removed third account

--- scripts/scripts.symlink/chat.sh
+++ scripts/scripts.symlink/chat.sh
@@ -12,7 +12,7 @@ else
     tmux new-session -d -s $SESSION 'profanity -a google'
     tmux selectp -t $SESSION
     tmux split-window -h 'profanity -a facebook'
-    tmux split-window -v 'profanity -a krz'
+    #tmux split-window -v 'profanity -a krz'
     tmux selectp -t 1
     tmux attach -t $SESSION
 fi
