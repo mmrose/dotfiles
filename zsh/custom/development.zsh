572ba64a5f800643c6d0a18f1fe88ca91b81e853
Martin Mrose <github@penthranet.de>
2015-04-28 08:37:47 +0200

    Made upr recursive

--- zsh/custom/development.zsh
+++ zsh/custom/development.zsh
@@ -5,13 +5,15 @@ alias ssb="svn log -v -r0:HEAD --stop-on-copy --limit 1"
 
 
 function upr() {
-    for DIR in $(find . -mindepth 1 -maxdepth 1 -type d); do
+    for DIR in $(find $PWD/ -mindepth 1 -maxdepth 1 -type d -not -path "$PWD/.vagrant" -not -path "$PWD/VMs" -not -path "$PWD/.metadata"); do
         cd $DIR
-        echo "Updating $DIR ..."
+        echo "Updating $DIR ...\n"
         if [[ -d '.git' ]]; then
             git pull
         elif [[ -d '.svn' ]]; then
             svn up
+        else
+            upr
         fi
         cd ..
     done
