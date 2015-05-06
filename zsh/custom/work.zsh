21cefa0fab08b411762b20fd1148c65fccc1614f
Martin Mrose <github@penthranet.de>
2015-05-06 12:45:02 +0200

    Replaced commands outside the box with commands inside the box

--- zsh/custom/work.zsh
+++ zsh/custom/work.zsh
@@ -13,31 +13,21 @@ if [[ `hostname` == W4DEUMSY9002045 ]]; then
 
     alias np='/cygdrive/c/"Program Files (x86)"/Notepad++/notepad++.exe'
     alias poedit="/cygdrive/c/Program\ Files\ \(x86\)/Poedit/Poedit.exe"
-
-    vms() {
-        tmux send-keys 'vagrant ssh app -- -t "cd /usr/local/*suite*; mediasuite/bin/manage runserver 8000" ";" exec /bin/zsh' C-m
-        tmux split-window
-        tmux send-keys "cd $PWD" C-m 'vagrant ssh app -- -t "cd /usr/local/*suite*; mediasuite/bin/manage runcelery beat --schedule=/vagrant/celerybeat-schedule.db --pidfile=/vagrant/celery_beat.pid" ";" exec /bin/zsh' C-m
-        tmux split-window
-        tmux send-keys "cd $PWD" C-m 'vagrant ssh app -- -t "cd /usr/local/*suite*; mediasuite/bin/manage runcelery worker -l fatal" ";" exec /bin/zsh' C-m
-    }
-    vws() {
-        tmux send-keys 'vagrant ssh app -- -t "cd /usr/local/*suite*; webcastsuite/bin/manage runserver 8100" ";" exec /bin/zsh' C-m
-        tmux split-window
-        tmux send-keys "cd $PWD" C-m 'vagrant ssh app -- -t "cd /usr/local/*suite*; webcastsuite/bin/manage runcelery beat --schedule=/vagrant/celerybeat-schedule.db --pidfile=/vagrant/celery_beat.pid" ";" exec /bin/zsh' C-m
-        tmux split-window
-        tmux send-keys "cd $PWD" C-m 'vagrant ssh app -- -t "cd /usr/local/*suite*; webcastsuite/bin/manage runcelery worker -Q highprio -l fatal -n highprio@%h" ";" exec /bin/zsh' C-m
-        tmux split-window
-        tmux send-keys "cd $PWD" C-m 'vagrant ssh app -- -t "cd /usr/local/*suite*; webcastsuite/bin/manage runcelery worker -Q celery -l fatal" ";" exec /bin/zsh' C-m
-    }
-    vwsi() {
-        tmux send-keys 'vagrant ssh dmz -- -t "cd /usr/local/*suite*; wsi/bin/manage runserver 8200" ";" exec /bin/zsh' C-m
-        tmux split-windo
-        tmux send-keys "cd $PWD" C-m 'vagrant ssh dmz -- -t "cd /var/local/*suite*; tail -f \$(find wsi/ -name *.log)" ";" exec /bin/zsh' C-m
-    }
 fi
 
 if [[ "`id -nu`" == "vagrant" ]]; then
+    alias msm="/usr/local/*/mediasuite/bin/manage"
+    alias wsm="/usr/local/*/webcastsuite/bin/manage"
+    alias wsim="/usr/local/*/wsi/bin/manage"
+
+    alias msr="while true; do msm runserver 8000; sleep 2; done"
+    alias msc="msm runcelery worker -BQ celery -l info --autoreload"
+    alias wsr="while true; do wsm runserver 8100; sleep 2; done"
+    alias wsc="wsm runcelery worker -BQ celery,highprio -l info --autoreload"
+    alias wsir="while true; do wsim runserver 8200; sleep 2; done"
+
+    alias sctl="/usr/local/*/supervisor/bin/supervisorctl"
+
     export STAGE=vagrant
     cd /vagrant 
 
