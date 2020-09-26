32abc28c258e650ef15e9da4645ae2f091873d80
Martin Mrose <martin@MARTIN-DELL.localdomain>
2020-09-27 00:33:30 +0200

    removed old install stuff and replaced it with removal of that

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-DOTFILES_DIR=".dotfiles"
-DOTFILES="$HOME/$DOTFILES_DIR"
 GIT_REPO="mmrose/dotfiles"
 GIT_BRANCH="master"
-OHMYZSH="$HOME/.oh-my-zsh"
 
 ## Check for git and curl
 if ! [ -x "$(command -v git)" ] || ! [ -x "$(command -v curl)" ]; then
@@ -22,109 +19,13 @@ for SYMLINK in $(find $HOME -type l); do
 done
 
 ## Grab oh-my-zsh
-if [ ! -d "$OHMYZSH" ]; then
-    git clone https://github.com/robbyrussell/oh-my-zsh.git $OHMYZSH
-else
-    cd $OHMYZSH
-    git pull
-    cd $HOME
+if [ -d "~/.oh-my-zsh" ]; then
+    rm -rf ~/.oh-my-zsh
 fi
 
 ## Grab dotfiles
-if [ ! -d "$DOTFILES" ]; then
-    git clone https://github.com/$GIT_REPO.git $DOTFILES
-    cd $DOTFILES
-    git checkout $BRANCH
-    git submodule init
-else
-    cd $DOTFILES
-    git pull
+if [ -d "~/.dotfiles" ]; then
+    rm -rf ~/.dotfiles
 fi
-git submodule update --init --recursive
-cd $HOME
-
-## Create placeholder directories if not already there
-for DIR in $(find $DOTFILES -not -iwholename "*.git*" -name "*.copy"); do
-    DIR_NAME="$(basename ${DIR%.copy})"
-    NEW_DIR="$HOME/.$DIR_NAME"
-    if [ ! -d "$NEW_DIR" ]; then
-        mkdir "$NEW_DIR"
-    fi
-    ## Symlink contents of placeholder directories (if they are directories)
-    if [ -d "$DIR" ]; then
-        for FILE in $(ls $DIR); do
-            NEW_FILE="$NEW_DIR/$FILE"
-            ## If not a symlink already
-            if [ ! -L "$NEW_FILE" ]; then
-                ## If the file exists (just not a symlink), back it up
-                if [ -e "$NEW_FILE" ]; then
-                    echo "Backup: $NEW_FILE -> $NEW_FILE.backup"
-                    mv "$NEW_FILE" "$NEW_FILE.backup"
-                fi
-                ## Create the symlink
-                echo "Creating symlink $NEW_FILE"
-                ln -s "$DIR/$FILE" "$NEW_FILE"
-            fi
-        done
-    fi
-done
-
-
-## Create config directories if not already there
-for MATCH in $(find $DOTFILES -not -iwholename "*.git*" -name "*.config"); do
-    BASE_NAME="$(basename ${MATCH%.config})"
-    TARGET="$HOME/.config/$BASE_NAME"
-    ## Symlink contents of placeholder directories (if they are directories)
-    if [ -d "$MATCH" ]; then
-        if [ ! -d "$TARGET" ]; then
-            mkdir -p "$TARGET"
-        fi
-        for FILE in $(ls $MATCH); do
-            NEW_FILE="$TARGET/$FILE"
-            ## If not a symlink already
-            if [ ! -L "$NEW_FILE" ]; then
-                ## If the file exists (just not a symlink), back it up
-                if [ -e "$NEW_FILE" ]; then
-                    echo "Backup: $NEW_FILE -> $NEW_FILE.backup"
-                    mv "$NEW_FILE" "$NEW_FILE.backup"
-                fi
-                ## Create the symlink
-                echo "Creating symlink $NEW_FILE"
-                ln -s "$MATCH/$FILE" "$NEW_FILE"
-            fi
-        done
-    else
-        ## If not symlink already
-        if [ ! -L "$TARGET" ]; then
-            ## Backup if file exists
-            if [ -e "$TARGET" ]; then
-                echo "Backup: $TARGET -> $TARGET.backup"
-                mv "$TARGET" "$TARGET.backup"
-            fi
-            ## Create symlink
-            echo "Creating symlink $TARGET"
-            ln -s "$MATCH" "$TARGET"
-        fi
-    fi
-done
-
-## Symlink all the things!
-for SYMLINK in $(find $DOTFILES -not -iwholename "*.git*" -name "*.symlink"); do
-    BASE_NAME="$(basename ${SYMLINK%.symlink})"
-    NEW_FILE="$HOME/.$BASE_NAME"
-    ## If the new file isn't a symlink
-    if [ ! -L "$NEW_FILE" ]; then
-        ## If the file already exists (just not a symlink), back it up
-        if [ -e "$NEW_FILE" ]; then
-            echo "Backup: $(basename $NEW_FILE) -> $(basename $NEW_FILE).backup"
-            mv "$NEW_FILE" "$NEW_FILE.backup"
-        fi
-        ## Create the symlink
-        echo "Creating symlink $NEW_FILE"
-        ln -s "$SYMLINK" "$NEW_FILE"
-    fi
-done
-
-vim +PluginInstall +qall
 
 exit 0;
